6331bd06c86f21a21e7711b99ab75f8006f16f46
Morgan McCrory <morganmccrory@gmail.com>
2015-04-11 15:37:52 -0500

    Implement Factory Girl

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
+user = FactoryGirl.build(:user)
+question = FactoryGirl.create(:question, user: user)
+answer = FactoryGirl.create(:answer, answerer: user, question_id: question.id)
+
 describe Answer do
-  let(:user) {User.create(username: "Test_User", email: "email@email.com", password: "password", location: "Chicago")}
-  let(:question) {user.questions.create(title: "What is Travis CI?", description: "Seriously, what is it?")}
-  let(:answer) {question.answers.create(content: "This is a sample answer! Lots of smart things here!", answerer: user)}
 
   it "should have content" do
     expect(answer.content).to eq("This is a sample answer! Lots of smart things here!")
