66fab7202460a0751fb848a4086f983c50aa9ed9
Durkheim <mosesyk.dev.bootcamp@gmail.com>
2015-04-12 14:18:45 -0500

    can now add comment to answer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,8 @@
 class CommentsController < ApplicationController
   def create
     @question = Question.find(params[:question_id])
-    @answer = Answer.find(params[:answer_id])
-    if @answer
+    if params[:answer_id]
+      @answer = Answer.find(params[:answer_id])
       @comment = @answer.comments.create(comment_params)
       @comment.update_attributes(commenter: current_user)
       redirect_to @question
