e79be29508026711d793bd6838a2cfcc6e712ec3
Durkheim <mosesyk.dev.bootcamp@gmail.com>
2015-04-12 17:19:31 -0500

    updating

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,51 +15,34 @@ class CommentsController < ApplicationController
 end
 
 def edit
-  if current_user
-    @question = Question.find(params[:question_id])
-    if params[:answer_id]
-      @answer = Answer.find(params[:answer_id])
-      @comment = @answer.comments.find(params[:id])
-    else
-      @comment = @question.comments.find(params[:id])
-    end
-  else
-    @question = Question.find(params[:question_id])
-    flash[:failure] = "Must be logged in to perform this action"
-    redirect_to @question
-  end
-end
-
-def update
   @question = Question.find(params[:question_id])
-  if params[:answer_id]
-     @answer = @question.answers.find(params[:answer_id])
-     @comment = @answer.comments.find(params[:id])
-     @comment.update_attributes(comment_params)
-
-  else
-    @comment = @question.comments.find(params[:id])
-    @comment.update_attributes(comment_params)
-  end
-   if @comment.save
-    redirect_to @question
-  else
-    render "edit"
-  end
-end
-
-def destroy
   if current_user
-    @question = Question.find(params[:question_id])
-    @answer = @question.answers.find(params[:id])
-    @answer.destroy
-    redirect_to @question
+    @comment = @question.comments.find(params[:id])
   else
     @question = Question.find(params[:question_id])
     flash[:failure] = "Must be logged in to perform this action"
     redirect_to @question
   end
 end
+
+# def update
+#   @question = Question.find(params[:question_id])
+#   if params[:answer_id]
+#      @answer = @question.answers.find(params[:answer_id])
+#      @comment = @answer.comments.find(params[:id])
+#      @comment.update_attributes(comment_params)
+
+#   else
+#     @comment = @question.comments.find(params[:id])
+#     @comment.update_attributes(comment_params)
+#   end
+#    if @comment.save
+#     redirect_to @question
+#   else
+#     render "edit"
+#   end
+# end
+
 
 
 
