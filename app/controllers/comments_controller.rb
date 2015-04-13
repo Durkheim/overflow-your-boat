65bbc524b93d35ce7246104fce991fad62170358
Morgan McCrory <morganmccrory@gmail.com>
2015-04-12 20:46:22 -0500

    Fix to not be able to post a comment if not logged in

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,20 @@
 class CommentsController < ApplicationController
   def create
-    @question = Question.find(params[:question_id])
-    if params[:answer_id]
-      @answer = Answer.find(params[:answer_id])
-      @comment = @answer.comments.create(comment_params)
-      @comment.update_attributes(commenter: current_user)
-      redirect_to @question
+    if current_user
+      @question = Question.find(params[:question_id])
+      if params[:answer_id]
+        @answer = Answer.find(params[:answer_id])
+        @comment = @answer.comments.create(comment_params)
+        @comment.update_attributes(commenter: current_user)
+        redirect_to @question
+      else
+        @comment = @question.comments.create(comment_params)
+        @comment.update_attributes(commenter: current_user)
+        redirect_to @question
+      end
     else
-      @comment = @question.comments.create(comment_params)
-      @comment.update_attributes(commenter: current_user)
+      @question = Question.find(params[:question_id])
+      flash[:failure] = "Must be logged in to perform this action"
       redirect_to @question
     end
   end
