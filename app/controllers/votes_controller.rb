73922a6b942586d79b6d80b2f5f118efb0cb5e88
Morgan McCrory <morganmccrory@gmail.com>
2015-04-12 20:34:59 -0500

    Add voting buttons

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,14 +3,14 @@ class VotesController < ApplicationController
     @question = Question.find(params[:question_id])
     if params[:answer_id] != nil
       @answer = Answer.find(params[:answer_id])
-      if params[:vote][:like] == "true"
+      if params[:like] == "true"
         @vote = @answer.votes.create(like: true)
       else
         @vote = @answer.votes.create(like: false)
       end
     redirect_to @question
     else
-      if params[:vote][:like] == "true"
+      if params[:like] == "true"
         @vote = @question.votes.create(like: true)
       else
         @vote = @question.votes.create(like: false)
